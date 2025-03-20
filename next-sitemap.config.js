6a3ff8c16499f54a76efa3e14368664d077a5b79
José Hernanes <68444005+devjoseh@users.noreply.github.com>
2025-03-19 23:41:46 -0300

    feat: sitemap fixed

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,13 +1,13 @@
 // next-sitemap.config.js
 module.exports = {
-    siteUrl: process.env.SITE_URL || "https://educaavalia.com.br",
+    siteUrl: "https://educaavalia.com.br",
     generateRobotsTxt: true,
+    sitemapSize: 7000,
     robotsTxtOptions: {
         policies: [
             { userAgent: "*", allow: "/" },
             { userAgent: "Googlebot-Image", allow: "/" },
         ],
-        additionalSitemaps: ["https://educaavalia.com.br/sitemap.xml"],
     },
     exclude: ["/server-sitemap.xml", "/404", "/admin/*"],
 };
